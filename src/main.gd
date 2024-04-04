be53e20e05ed5739a5ddb315339d7c46cf8c130d
Jed <avisucam@gmail.com>
2024-04-04 16:15:00 +0800

    Add main script. Set xr settings on run.

---
+++ src/main.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var xr_interface: XRInterface
+
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialised successfully.")
+		ProjectSettings.set_setting("xr/openxr/enabled", true)
+		ProjectSettings.set_setting("xr/shaders/enabled", true)
+		
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, starting in non-VR mode.")
