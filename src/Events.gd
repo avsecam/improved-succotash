716389c9b00ce79dca1bb3c71eb29753173c2670
Jed <avisucam@gmail.com>
2024-04-04 16:15:13 +0800

    Add Events autoload.

---
+++ src/Events.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+# TELEPORT TRIGGER
+signal player_teleport_requested_trigger(new_panorama_file_path: String)
+signal set_player_rotation_requested(current_rotation: Vector3, new_rotation: Vector3)
