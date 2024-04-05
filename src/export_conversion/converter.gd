cbb50d023f47e09d13280cf1ee454e4ade187cd8
Jed <avisucam@gmail.com>
2024-04-05 12:08:39 +0800

    Convert data into teleporter data instead of actual Teleporter nodes. Don't save image with scene.

--- src/export_conversion/converter.gd
+++ src/export_conversion/converter.gd
@@ -1,30 +1,45 @@
-extends Object
+@tool
+extends EditorScript
 
 const AREAS_DIR = "res://src/areas/"
 
-func convertToScene(json_data: String):
+func _run():
+	var files_dir = DirAccess.open(AREAS_DIR)
+	var files = files_dir.get_files()
+	
+	for file in files:
+		if file.ends_with(".json"):
+			var panorama = convertToScene(file)
+
+			var scene = PackedScene.new()
+			scene.pack(panorama)
+			ResourceSaver.save(scene, AREAS_DIR + file.replace(".json", ".tscn"))
+
+			print("Converted " + file)
+
+func convertToScene(filename: String) -> Panorama:
+	var json_data = FileAccess.get_file_as_string(AREAS_DIR + filename)
+	
 	var data = JSON.parse_string(json_data)
 
-	var panorama = preload("res://src/teleportation/Panorama.tscn").instantiate()
+	var panorama = preload ("res://src/teleportation/Panorama.tscn").instantiate()
 
 	panorama.name = data.area_name
 	
-	# Set 360 image
-	var image = Image.load_from_file(AREAS_DIR + (data.panorama_texture_filename))
-	var texture = ImageTexture.create_from_image(image)
-	((panorama.mesh_instance.mesh as SphereMesh).material as StandardMaterial3D).albedo_texture = texture
+	# Set 360 image filename
+	panorama.image_filename = (AREAS_DIR + (data.panorama_texture_filename))
 
 	# Set teleporters
-	for teleporter_position in data.teleporter_positions:
-		var teleporter = preload("res://src/teleportation/Teleporter.tscn").instantiate()
-		
-		teleporter.position = Vector3(
-			teleporter_position.position.x,
-			teleporter_position.position.y,
-			teleporter_position.position.z
+	for teleporter_data in data.teleporter_positions:
+		var new_data = {}
+		new_data.position = Vector3(
+			teleporter_data.position_x,
+			teleporter_data.position_y,
+			teleporter_data.position_z
 		)
 
-		teleporter.to = teleporter_position.teleport_location_filename
+		new_data.to = (teleporter_data.teleport_location_filename as String).get_slice(".json", 0)
 
-		panorama.teleporters.add_child(teleporter)
+		panorama.teleporters_data.append(new_data)
 	
+	return panorama
