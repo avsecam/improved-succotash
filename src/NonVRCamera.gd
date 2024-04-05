6a1060fddba4e120d1ee2f6be8dd66ce134f7f38
Jed <avisucam@gmail.com>
2024-04-05 12:12:07 +0800

    Add Non VR Camera scene.

---
+++ src/NonVRCamera.gd
@@ -0,0 +1,10 @@
+extends Camera3D
+
+const ROTATION_SPEED = 0.01
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("ui_left"):
+		self.rotate_y(ROTATION_SPEED)
+	if Input.is_action_pressed("ui_right"):
+		self.rotate_y(-ROTATION_SPEED)
+		
