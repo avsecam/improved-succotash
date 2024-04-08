80f0d901c0976826547b702389cdf06f54303dad
jed <jljocson@ateneo.edu>
2024-04-08 15:20:46 +0900

    Fix handling of startup (checking if VR is available).

--- src/main.gd
+++ src/main.gd
@@ -6,14 +6,12 @@ func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR initialised successfully.")
-		
-		
-		ProjectSettings.set_setting("xr/openxr/enabled", true)
-		ProjectSettings.set_setting("xr/shaders/enabled", true)
-		
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		
 		get_viewport().use_xr = true
+		Events.start_with_vr.emit()
 	else:
+		ProjectSettings.set_setting("xr/openxr/enabled", false)
+		ProjectSettings.set_setting("xr/shaders/enabled", false)
 		print("OpenXR not initialized, starting in non-VR mode.")
 		Events.start_without_vr.emit()
