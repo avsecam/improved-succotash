6819e2c29cea620a6a9ccb3f3b57a94b19a9eacc
joan <55185980+joan-lee@users.noreply.github.com>
2024-04-11 15:23:32 +0800

    Add wipe action

---
+++ src/ois/action_components/wipe_action.gd
@@ -0,0 +1,32 @@
+extends ReceiverObj
+
+var interacting_inital_pos
+var delta_dist_prev = 0
+var total_delta_dist = 0
+var buffer = 0.005
+var past_progress = 0
+
+func initialize_action_vars():
+	interacting_inital_pos = interacting_object.position
+	past_progress = total_progress
+	
+	delta_dist_prev = 0
+	total_delta_dist = 0
+
+func _process(_delta):
+	var interacting_current_pos = interacting_object.position
+	
+	var delta_dist = interacting_inital_pos.distance_to(interacting_current_pos)
+	#print(str(interacting_inital_pos) + " - " + str(interacting_current_pos) + " = " + str(delta_dist))
+	var current_progress = total_delta_dist + delta_dist
+		
+	if(delta_dist < (delta_dist_prev-buffer)):
+		total_delta_dist += delta_dist
+		interacting_inital_pos = interacting_object.position
+	
+	delta_dist_prev = delta_dist
+	
+	total_progress = past_progress + (current_progress * rate);
+	print(total_progress)
+	# action_performed.emit(requirement, total_progress)
+	check_if_completed()
