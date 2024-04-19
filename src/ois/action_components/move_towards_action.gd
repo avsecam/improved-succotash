b55af078e7f01c677f536d19c14baeee101d403f
joan <55185980+joan-lee@users.noreply.github.com>
2024-04-20 01:44:19 +0800

    Add pulling/move towards action

---
+++ src/ois/action_components/move_towards_action.gd
@@ -0,0 +1,23 @@
+extends ReceiverObj
+
+@export var reference_point : Node3D
+@export var req_is_init_dist : bool = false
+var past_progress = 0
+var initial_distance
+
+func _ready():
+	super()
+	if req_is_init_dist:
+		requirement = global_position.distance_to(reference_point.global_position)
+
+func initialize_action_vars():
+	initial_distance = interacting_object.position.distance_to(reference_point.position)
+	past_progress = total_progress
+	
+func _process(delta):
+	var curr_distance = interacting_object.position.distance_to(reference_point.position)
+	var current_progress = initial_distance-curr_distance
+	total_progress = past_progress + (current_progress * rate)
+	$Label3D.set_text("req: %s\ncurr: %s\ntotal: %s" % [requirement, current_progress, total_progress])
+	
+	check_if_completed()
