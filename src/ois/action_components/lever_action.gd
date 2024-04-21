4caf319c89620c2238f9ca0e1597805eaa021f9b
joan <55185980+joan-lee@users.noreply.github.com>
2024-04-21 15:29:22 +0800

    Add lever action and fix rotation feedback to account for starting transformations

---
+++ src/ois/action_components/lever_action.gd
@@ -0,0 +1,36 @@
+extends ReceiverObj
+
+var initial_direction
+var past_progress = 0
+
+var init_direction2
+var action_comp_origin2
+var init_handle_pos2
+var curr_handle_pos2
+var delta_angle_buffer2 = 0
+var total_progress2 = 0
+var past_progress2 = 0
+
+func initialize_action_vars():
+	initial_direction = interacting_object.position-position
+	past_progress = total_progress
+	
+	delta_angle_buffer2 = 0
+	action_comp_origin2 = position
+	init_handle_pos2 = interacting_object.position
+	init_direction2 = init_handle_pos2-action_comp_origin2
+	init_direction2.x = 0
+	past_progress2 = total_progress2
+	set_process(true)
+
+
+func _process(delta):
+	var curr_direction = interacting_object.position-position 
+	
+	var axis = Vector3(0, 0, 1)
+	var current_progress = initial_direction.signed_angle_to(curr_direction, axis)
+	
+	total_progress = past_progress + (current_progress * rate);
+	print(total_progress)
+	action_performed.emit(requirement, total_progress)
+	check_if_completed()
--- src/ois/feedback_components/rotate_self.gd
+++ src/ois/feedback_components/rotate_self.gd
@@ -2,6 +2,13 @@ extends Feedback
 
 @export var axis : Vector3 = Vector3(0, 0, 1)
 
+var init_transform
+
+func _ready():
+	super()
+	init_transform = receiver_object.transform.basis
+	print(init_transform)
+	
 func show_feedback(requirement, total_progress):
-	receiver_object.transform.basis = Basis()
+	receiver_object.transform.basis = init_transform
 	receiver_object.rotate_object_local(axis, total_progress)
