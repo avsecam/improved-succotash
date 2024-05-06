d4c374c9a4175fab58d75d0ebfc39feefe496ae4
joan <55185980+joan-lee@users.noreply.github.com>
2024-05-06 15:22:12 +0800

    Add error message to state manager when initial state not set

--- src/ois/state_managers/state_manager.gd
+++ src/ois/state_managers/state_manager.gd
@@ -9,11 +9,15 @@ var receiver_object
 @onready var actor = get_parent()
 
 func _ready():
+	await actor.ready
 	set_state(current_state)
 
 func set_state(state : OISState):
 	if (current_state != null):
 		current_state.exit_state.emit()
+	else:
+		print("State Manager Error: Set current state of object (initial state)")
+		push_error("State Manager Error: Set current state of object (initial state)")
 	if debug:
 		print(actor.name + ": " + current_state.name + " -> " + state.name)
 	current_state = state
