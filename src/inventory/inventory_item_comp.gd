e852fabec2b0f781ffd98b3dab907ea98081be8f
joan <55185980+joan-lee@users.noreply.github.com>
2024-05-23 17:21:05 +0800

    Create inventory item comp - can be added to pickable obj to make it inventory (instead of prev inventory item which extends pickable obj and need to inherit from when making inventory obj)

---
+++ src/inventory/inventory_item_comp.gd
@@ -0,0 +1,38 @@
+@tool
+extends Node3D
+
+@onready var main_mesh = get_parent().get_node_or_null("MainMesh")
+@onready var replacement_mesh = $ReplacementMesh
+
+@onready var state_manager = get_parent().get_node_or_null("StateManager")
+
+func _ready():
+	toggle_replacement_mesh()
+	get_parent().add_to_group("Objects")
+
+func toggle_replacement_mesh() -> void:
+	print(self.name+" | Toggled replacement mesh OFF.")	
+	main_mesh.visible = true
+	replacement_mesh.visible = false
+	
+func toggle_main_mesh() -> void:
+	print(self.name+" | Toggled main mesh OFF.")	
+	main_mesh.visible = false
+	replacement_mesh.visible = true
+	if state_manager != null:
+		state_manager.add_to_inventory()
+
+func _get_configuration_warnings():
+	var warnings = []
+	
+	var has_grab_point = false
+	for i in get_parent().get_children():
+		if i is XRToolsGrabPointHand:
+			has_grab_point = true
+			break
+	
+	if !has_grab_point:
+		warnings.append("Add GrabPointHand")
+
+	# Returning an empty array means "no warning".
+	return warnings
--- src/inventory/inventory_snap_zone.gd
+++ src/inventory/inventory_snap_zone.gd
@@ -1,9 +1,19 @@
 extends XRToolsSnapZone
 
 var current_inventory_obj
+var current_inventory_item_comp
+
+# TODO: Consider transition from using inventory_item objects to using inventory_item_comp
+# If one wants to make object inventory object, just add inventory_item_comp
 
 func _on_has_dropped():
-	current_inventory_obj.toggle_replacement_mesh()
+	# handles objects using inventory_item scene (old)
+	if current_inventory_obj.has_method("toggle_replacement_mesh"):
+		current_inventory_obj.toggle_replacement_mesh()
+	# handles objects using inventory_item_comp (new)
+	elif current_inventory_item_comp != null:
+		current_inventory_item_comp.toggle_main_mesh()
+	
 	# Transfer object parent from snapzone to actual slot.
 	self.get_parent().remove_child(current_inventory_obj)
 	# If object is currently in slot, remove parent before adding to World.
@@ -11,13 +21,23 @@ func _on_has_dropped():
 		current_inventory_obj.get_parent().remove_child(current_inventory_obj)
 	# index is set to 1 because XRUserSettings is child 0 -- will check if there's a better implementation
 	get_tree().get_root().get_child(1).add_child(current_inventory_obj)
-
+	
+	#current_inventory_obj = null
+	current_inventory_item_comp = null
 
 func _on_has_picked_up(picked_up_object):
 	current_inventory_obj = picked_up_object
 	print("Picked up " + current_inventory_obj.name)
-	current_inventory_obj.toggle_main_mesh()
 	
+	current_inventory_item_comp = current_inventory_obj.get_node_or_null("InventoryItemComp")
+	
+	# handles objects using inventory_item scene (old)
+	if current_inventory_obj.has_method("toggle_main_mesh"):
+		current_inventory_obj.toggle_main_mesh()
+	# handles objects using inventory_item_comp (new)
+	elif current_inventory_item_comp != null:
+		current_inventory_item_comp.toggle_main_mesh()
+		
 	# Add as child of node to ensure better movement
 	current_inventory_obj.get_parent().remove_child(current_inventory_obj)
 	self.get_parent().get_node("Inventory Content").add_child(current_inventory_obj)
