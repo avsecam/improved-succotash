53cf7e7d7d8c87aba1b07df0e38d6e0cfeee4b65
joan <55185980+joan-lee@users.noreply.github.com>
2024-05-29 19:07:57 +0800

    Refine gizmo selection buttons layout

--- src/authoring_tools/gizmos/Editor_UI.gd
+++ src/authoring_tools/gizmos/Editor_UI.gd
@@ -12,10 +12,10 @@ func _ready():
 	
 	# Connect the "pressed" signals from the four gizmo buttons to the on_mode_button_pressed function. We pass in a additional
 	# argument along with the signal, which is the name of the mode we want to change to.
-	get_node("Gizmo_Selection/HBoxContainer/Button_Select").pressed.connect(on_mode_button_pressed.bind("SELECT"));
-	get_node("Gizmo_Selection/HBoxContainer/Button_Translate").pressed.connect(on_mode_button_pressed.bind("TRANSLATE"));
-	get_node("Gizmo_Selection/HBoxContainer/Button_Rotate").pressed.connect(on_mode_button_pressed.bind("ROTATE"));
-	get_node("Gizmo_Selection/HBoxContainer/Button_Scale").pressed.connect(on_mode_button_pressed.bind("SCALE"));
+	get_node("MarginContainer/Gizmo_Selection/Button_Select").pressed.connect(on_mode_button_pressed.bind("SELECT"));
+	get_node("MarginContainer/Gizmo_Selection/Button_Translate").pressed.connect(on_mode_button_pressed.bind("TRANSLATE"));
+	get_node("MarginContainer/Gizmo_Selection/Button_Rotate").pressed.connect(on_mode_button_pressed.bind("ROTATE"));
+	get_node("MarginContainer/Gizmo_Selection/Button_Scale").pressed.connect(on_mode_button_pressed.bind("SCALE"));
 
 func on_mode_button_pressed(new_mode):
 	# Tell editor_controller to change the mode.
