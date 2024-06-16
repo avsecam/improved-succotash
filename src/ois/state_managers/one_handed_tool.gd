2e7b93b5bd83bad442949ab82b0a205ba36799eb
joan <55185980+joan-lee@users.noreply.github.com>
2024-06-16 14:07:34 +0800

    Add support for multiple receiver components

--- src/ois/state_managers/one_handed_tool.gd
+++ src/ois/state_managers/one_handed_tool.gd
@@ -58,6 +58,12 @@ func _on_receiver_collision_entered(receiver):
 	if receiver.is_in_group(receiver_group):
 		receiver_object = receiver
 		set_state(current_state)
+	else:
+		for child in receiver.get_children():
+			if child.is_in_group(receiver_group):
+				receiver_object = child
+				set_state(current_state)
+				break
 
 func _on_receiver_collision_exited(receiver):
 	if (current_state == active_state):
--- src/ois/state_managers/two_handed_tool.gd
+++ src/ois/state_managers/two_handed_tool.gd
@@ -92,6 +92,12 @@ func _on_receiver_collision_entered(receiver):
 	if receiver.is_in_group(receiver_group):
 		receiver_object = receiver
 		set_state(current_state)
+	else:
+		for child in receiver.get_children():
+			if child.is_in_group(receiver_group):
+				receiver_object = child
+				set_state(current_state)
+				break
 
 func _on_receiver_collision_exited(receiver):
 	if (current_state == active1_state || current_state == active2_state):
