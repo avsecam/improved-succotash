6747a4a6777d027143d985bde31662a8476bda80
joan <55185980+joan-lee@users.noreply.github.com>
2024-06-26 18:48:34 +0800

    Add confirmation dialogue for leaving without saving area

--- src/authoring_tools/level_editor/object_positioning.gd
+++ src/authoring_tools/level_editor/object_positioning.gd
@@ -9,7 +9,13 @@ var current_area_path : String
 @onready var new_obj_initial_pos = $Editor_Controller/Editor_Camera_Controller/View_Camera/NewObjInitialPos
 @onready var notification_panel = $NotificationPanel
 
+@onready var cd_proceed_without_saving = $CDProceedWithoutSaving
+var load_area_func = func(): $FDLoadArea.visible = true
+var close_window_func = func(): get_tree().quit()
+var has_unsaved_changes = false
+
 func _ready():
+	get_tree().set_auto_accept_quit(false)
 	$FDLoadArea.visible = false
 	$FDAddItem.visible = false
 	_on_btn_gravity_on_toggled(false)
@@ -22,12 +28,22 @@ func _ready():
 		current_area_path = current_area.scene_file_path
 		print(current_area_path)
 
+func edit_scene():
+	has_unsaved_changes = true
+
 func _on_btn_load_area_pressed():
-	$FDLoadArea.visible = true
+	if has_unsaved_changes:
+		if cd_proceed_without_saving.confirmed.is_connected(close_window_func):
+			cd_proceed_without_saving.confirmed.disconnect(close_window_func)
+		cd_proceed_without_saving.confirmed.connect(load_area_func)
+		cd_proceed_without_saving.visible = true
+	else:
+		load_area_func.call()
 
 func _on_fd_load_area_file_selected(path):
 	$Editor_Controller/Editor_Viewport.physics_object_selected(null)
 	current_area = panorama_container.set_scene(path)
+	has_unsaved_changes = false
 	if current_area != null:
 		current_area_path = path
 
@@ -40,6 +56,7 @@ func _on_fd_add_item_file_selected(path):
 		current_area.add_child(new_obj)
 		new_obj.owner = current_area
 		new_obj.global_position = new_obj_initial_pos.global_position
+		edit_scene()
 
 func _on_btn_overwrite_save_pressed():
 	$CDOverwriteSave.visible = true
@@ -56,6 +73,7 @@ func _on_cd_overwrite_save_confirmed():
 	else:
 		print("Saved area to %s" % current_area_path)
 		notification_panel.show_notification("Saved area to %s" % current_area_path)
+		has_unsaved_changes = false
 
 func _on_btn_save_as_pressed():
 	$FDSaveArea.visible = true
@@ -71,6 +89,7 @@ func _on_fd_save_area_file_selected(path):
 	else:
 		print("Saved object to %s" % path)
 		notification_panel.show_notification("Saved object to %s" % path)
+		has_unsaved_changes = false
 
 func _on_btn_fixed_view_toggled(toggled_on):
 	camera_controller.is_panorama_camera = toggled_on
@@ -81,7 +100,16 @@ func _on_btn_gravity_on_toggled(toggled_on):
 func _on_btn_view_floor_toggled(toggled_on):
 	$Floor/MeshInstance3D.visible = toggled_on
 
-
 func _on_btn_delete_obj_pressed():
 	$Editor_Controller/Editor_Viewport.selected_physics_object.queue_free()
 	$Editor_Controller/Editor_Viewport.physics_object_selected(null)
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		if has_unsaved_changes:
+			if cd_proceed_without_saving.confirmed.is_connected(load_area_func):
+				cd_proceed_without_saving.confirmed.disconnect(load_area_func)
+			cd_proceed_without_saving.confirmed.connect(close_window_func)
+			cd_proceed_without_saving.visible = true
+		else:
+			close_window_func.call()
