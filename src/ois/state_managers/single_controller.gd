61b31550dec463b2aa9028b4f5cdb9fbef4ff09e
joan <55185980+joan-lee@users.noreply.github.com>
2024-06-28 06:39:46 +0800

    Add both_controllers state manager, fix single_controller bug (if exit collision while in trigger mode, still interacts with object for as long as raycast does not hit anything else)

--- src/ois/state_managers/single_controller.gd
+++ src/ois/state_managers/single_controller.gd
@@ -1,7 +1,8 @@
 @tool
 extends StateManager
+class_name SingleControllerSM
 
-@export var controller : XRController3D
+var controller : XRController3D
 
 @onready var idle_state = $IdleState
 @onready var active_state = $ActiveState
@@ -10,12 +11,23 @@ extends StateManager
 
 @onready var actor_object = get_parent()
 
+var is_colliding = false
+
+signal change_state(state_manager, state)
+signal body_entered(state_manager, body)
+signal body_exited(state_manager, body)
+
 func _ready():
 	super()
+	controller = get_parent()
 	if controller != null:
 		controller.button_pressed.connect(func(x): if (x == "trigger_click"): _on_trigger_pressed())
 		controller.button_released.connect(func(x): if (x == "trigger_click"): _on_trigger_released())
-	
+
+func set_state(state : OISState):
+	super(state)
+	change_state.emit(self, state)
+
 # triggered by object state managers
 func _on_object_released():
 	set_state(active_state)
@@ -30,15 +42,28 @@ func _on_trigger_pressed():
 
 func _on_trigger_released():
 	if (current_state == trigger_state):
-		set_state(active_colliding_state)
+		if is_colliding:
+			set_state(active_colliding_state)
+		else:
+			set_state(active_state)
 
 func _on_receiver_collision_entered(receiver):
+	is_colliding = true
+	body_entered.emit(self, receiver)
+	
 	if (current_state == active_state):
 		if receiver.is_in_group(receiver_group):
 			receiver_object = receiver
 			set_state(active_colliding_state)
 
 func _on_receiver_collision_exited(receiver):
+	# To make interaction reqs less strenous, don't automatically transition out of interacting state
+	# when collision is exited (as it may be difficult for users to remain precision)
+	# Instead, keep track whether it is colliding so that upon next transition, can act accordingly
+	is_colliding = false
+	body_exited.emit(self, receiver)
+	
 	if (current_state == active_colliding_state):
-		receiver_object = null
-		set_state(active_state)
+		if receiver.is_in_group(receiver_group):
+			receiver_object = null
+			set_state(active_state)
