534b610fbf08039f5dcded22744d3c00c0e2fe70
kennethkingko <kennethkingko@gmail.com>
2024-08-21 20:18:05 +0800

    cat hovered effects

--- src/ois/action_components/wipe_action.gd
+++ src/ois/action_components/wipe_action.gd
@@ -5,6 +5,15 @@ var delta_dist_prev = 0
 var total_delta_dist = 0
 var buffer = 0.005
 var past_progress = 0
+@onready var mesh = $MeshInstance3D2
+var default_material : Material
+const white_hover = preload("res://src/ois/xx_demo_objects/white.tres")
+const normal_hover = preload("res://src/ois/xx_demo_objects/normal.tres")
+
+#func _ready():
+	# Initialize materials
+	#default_material = mesh.material_override
+	
 
 func initialize_action_vars():
 	interacting_inital_pos = interacting_object.position
@@ -30,3 +39,20 @@ func _process(_delta):
 	print(total_progress)
 	
 	super(_delta)
+
+# Handle pointer events
+func pointer_event(event : XRToolsPointerEvent) -> void:
+	if event.event_type == XRToolsPointerEvent.Type.ENTERED:
+		# When the pointer enters, set the hover material
+		print("hovering at cat")
+		#mesh.material_override = hover_material
+
+	elif event.event_type == XRToolsPointerEvent.Type.EXITED:
+		# When the pointer exits, revert to the default material
+		print("no longer hovering at cat")
+		mesh.material_override = null
+
+	elif event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		# You can still handle pressing if needed
+		print("pointing at cat")
+		mesh.material_override = white_hover
