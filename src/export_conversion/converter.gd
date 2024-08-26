0e48859bd11d2351ac32e409671db06ae8fe4994
Jed <avisucam@gmail.com>
2024-08-26 12:37:52 +0800

    Adjust converter to cater to 3D scenes.

--- src/export_conversion/converter.gd
+++ src/export_conversion/converter.gd
@@ -8,29 +8,35 @@ func _run():
 	var files = files_dir.get_files()
 	
 	for file in files:
-		if file.ends_with(".json"):
-			var panorama = convertToScene(file)
+		if file.ends_with(".json") and file.contains("obj"):
+			var level = convertToScene(file)
 
 			var scene = PackedScene.new()
-			scene.pack(panorama)
+			scene.pack(level)
 			ResourceSaver.save(scene, AREAS_DIR + file.replace(".json", ".tscn"))
 
 			print("Converted " + file)
 
-func convertToScene(filename: String) -> Panorama:
+func convertToScene(filename: String) -> Level:
 	var json_data = FileAccess.get_file_as_string(AREAS_DIR + filename)
 	
 	var data = JSON.parse_string(json_data)
+	print(data)
 
-	var panorama: Panorama = preload ("res://src/teleportation/Panorama.tscn").instantiate()
-
-	panorama.name = data.area_name
+	var level: Level
 	
-	# Set 360 image filename
-	panorama.image_filename = (AREAS_DIR + (data.panorama_texture_filename))
+	if data.mesh_filename:
+		level = preload("res://src/teleportation/ThreeDScene.tscn").instantiate()
+		level.obj_filename = (AREAS_DIR + (data.mesh_filename))
+	else:
+		level = preload("res://src/teleportation/Panorama.tscn").instantiate()
+		# Set 360 image filename
+		level.image_filename = (AREAS_DIR + (data.panorama_texture_filename))
+
+	level.name = data.area_name
 	
 	# Set base rotation
-	panorama.base_rotation = data.base_rotation
+	level.base_rotation = data.base_rotation
 
 	# Set teleporters
 	for teleporter_data in data.teleporter_positions:
@@ -43,6 +49,18 @@ func convertToScene(filename: String) -> Panorama:
 
 		new_data.to = (teleporter_data.teleport_location_filename as String).get_slice(".json", 0)
 
-		panorama.teleporters_data.append(new_data)
+		level.teleporters_data.append(new_data)
 	
-	return panorama
+	for teleporter_data in data.teleport_spots:
+		var new_data = {}
+		new_data.position = Vector3(
+			teleporter_data.position_x,
+			teleporter_data.position_y,
+			teleporter_data.position_z
+		)
+
+		level.teleporters_data.append(new_data)
+	
+	print(level.teleporters_data)
+	
+	return level
