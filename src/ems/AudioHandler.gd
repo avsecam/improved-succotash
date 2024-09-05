cfb8da52341d83b493ec707a4e5133c9f0b0dc80
Alvbatross <alvaropato54@gmail.com>
2024-09-05 11:04:17 +0800

    Made it so the AudioHandler plays the dialogue_source instead

--- src/ems/AudioHandler.gd
+++ src/ems/AudioHandler.gd
@@ -29,9 +29,13 @@ func play_bgm(bgm_key):
 	bgm_player.play()
 
 func play_dialogue(dialogue_key, dialogue_source):
-	dialogue_player = dialogue_source
-	dialogue_player.stream = dialogue_locations[dialogue_key]
-	dialogue_player.play()
+	#dialogue_player = dialogue_source
+	#dialogue_player.stream = dialogue_locations[dialogue_key]
+	#dialogue_player.play()
+	dialogue_source.stream = dialogue_locations[dialogue_key]
+	dialogue_source.play()
+	await dialogue_source.finished
+	dialogue_player.emit_signal("finished")
 
 
 func _on_bgm_player_finished():
--- src/ems/eventsystem_new/BGMPlayerOutside.gd
+++ src/ems/eventsystem_new/BGMPlayerOutside.gd
@@ -2,3 +2,4 @@ extends Event
 
 func _on_event_started():
 	play_event_audio()
+	 
