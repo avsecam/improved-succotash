8c95c0a5b5a7c40c8597db6c8505813d81f48247
kennethkingko <kennethkingko@gmail.com>
2024-09-06 11:13:30 +0800

    fixed visibility of items problems in Tut2 and 3

--- src/ems/eventsystem_new/ActionGetCatPicture.gd
+++ src/ems/eventsystem_new/ActionGetCatPicture.gd
@@ -1,8 +1,12 @@
 extends Event
 
+@onready var frame = $"../../Frame"
+
 func _on_event_started():
 	await get_tree().create_timer(loop_interval).timeout
+	frame.visible = false
 	play_event_audio()
+	frame.visible = true
 
 func _on_frame_picked_up(pickable):
 	close_event()
