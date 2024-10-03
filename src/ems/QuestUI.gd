a50a7525ee2768f86e82a60f8e5ae96000a2a65f
Alvbatross <alvaropato54@gmail.com>
2024-10-03 10:36:26 +0800

    Made a few modifications to dialogue and quest tracker

--- src/ems/QuestUI.gd
+++ src/ems/QuestUI.gd
@@ -3,6 +3,7 @@ class_name QuestUI
 
 @onready var quest_name_label := $QuestName
 @onready var quest_actions_container := $MarginContainer/QuestActions
+@export var quest_action_label := preload("res://src/ems/QuestActionLabel.tscn")
 
 var quest_name : String
 var quest_actions : Dictionary
@@ -22,9 +23,9 @@ func initialize_quest_ui(qn : String, qa : Dictionary) -> void:
 	quest_name_label.set_text(Events.quest_library[quest_name]["Quest_Description"]["Name"])
 	
 	for acts in quest_actions:
-		var new_action = Label.new()
+		var new_action = quest_action_label.instantiate()
 		if quest_actions[acts].size() > 1:
-			new_action.set_text("- " + acts + " " + str(quest_actions[acts].size()) + "x : 0/" + str(quest_actions[acts].size()))
+			new_action.set_text("- " + acts + " : 0/" + str(quest_actions[acts].size()))
 		else:
 			new_action.set_text("- " + acts)
 		print("added quest")
@@ -36,16 +37,16 @@ func update_quest_ui() -> void:
 		child.queue_free()
 	
 	for acts in quest_actions:
-		var new_action = Label.new()
+		var new_action = quest_action_label.instantiate()
 		if quest_actions[acts].size() > 1:
 			var iterator : int = 0
 			for a in quest_actions[acts]:
 				if a in Events.finished_events:
 					iterator += 1
 			if iterator < quest_actions[acts].size():
-				new_action.set_text("- " + acts + " " + str(quest_actions[acts].size()) + "x : " + str(iterator) + "/" + str(quest_actions[acts].size()))
+				new_action.set_text("- " + acts + " : " + str(iterator) + "/" + str(quest_actions[acts].size()))
 			elif iterator >= quest_actions[acts].size():
-				new_action.set_text("- " + acts + str(quest_actions[acts].size()) + "x : ✓")
+				new_action.set_text("- " + acts + " : ✓")
 		else:
 			if quest_actions[acts][0] in Events.finished_events:
 				new_action.set_text("- " + acts + " : ✓")
--- src/ems/eventsystem_new/ActionPetTheCat.gd
+++ src/ems/eventsystem_new/ActionPetTheCat.gd
@@ -4,9 +4,9 @@ extends Event
 @onready var cat := $"../../Cat"
 
 func _on_event_started():
-	await get_tree().create_timer(loop_interval).timeout
 	cat.action_completed.connect(_on_cat_action_ended)
 	cat_hitbox.disabled = false
+	await get_tree().create_timer(loop_interval).timeout
 	play_event_audio()
 	
 func _on_cat_pet_event_done():
