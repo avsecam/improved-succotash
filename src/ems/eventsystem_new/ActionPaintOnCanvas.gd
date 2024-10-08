cc48701323804b47c5b0af99e8beaa4fb7e64c2c
kennethkingko <kennethkingko@gmail.com>
2024-10-08 20:47:14 +0800

    B3 painting test and questline test on Tut1
    Will be moved later

---
+++ src/ems/eventsystem_new/ActionPaintOnCanvas.gd
@@ -0,0 +1,8 @@
+extends Event
+
+func _on_canvas_paint_action_ended(requirement, total_progress):
+	close_event()
+
+
+func _on_canvas_paint_action_completed(requirement, total_progress):
+	close_event() # Replace with function body.
