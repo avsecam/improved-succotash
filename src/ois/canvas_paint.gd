72c14f22839847720f0bb196e26ca8a607764f4b
kennethkingko <kennethkingko@gmail.com>
2024-10-09 16:30:12 +0800

    B3 finalization before transfer to actual room
    Just needs a final quest bug fix.

---
+++ src/ois/canvas_paint.gd
@@ -0,0 +1,32 @@
+extends WipeAction
+
+@onready var anim := $AnimationPlayer
+@onready var canvas_hitbox := $InteractArea/CollisionShape3D
+@onready var canvas_blank := $MainMesh/Canvas_Blank
+@onready var canvas_ahrt := $MainMesh/Canvas_Ahrt
+
+signal canvas_disappear_signal
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	canvas_hitbox.disabled = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func canvas_disappear():
+	anim.play("FlyUpAnimation")
+	canvas_ahrt.visible = true
+	canvas_blank.visible = false
+	await get_tree().create_timer(2.0).timeout
+	self.queue_free()
+	canvas_disappear_signal.emit()
+	
+
+func _on_interact_area_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		canvas_disappear()
+	elif event.event_type == XRToolsPointerEvent.Type.ENTERED:
+		print("ASDKJASDKLASDJLASKDJASLKDJASLKDJASDLKJASDLKAJSDLAKSDJASLKDJ")
+	
