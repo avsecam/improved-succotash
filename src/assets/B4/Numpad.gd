730d66add000f59e1e210709ce31638e17817f3a
kennethkingko <kennethkingko@gmail.com>
2024-10-17 11:29:43 +0800

    numpad system added, needs integration in the game puzzles

---
+++ src/assets/B4/Numpad.gd
@@ -0,0 +1,46 @@
+extends Node3D
+
+
+func _on_numpad_1_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(1)
+
+
+func _on_numpad_2_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(2)
+
+
+func _on_numpad_3_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(3)
+
+
+func _on_numpad_4_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(4)
+
+
+func _on_numpad_5_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(5)
+
+
+func _on_numpad_6_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(6)
+
+
+func _on_numpad_7_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(7)
+
+
+func _on_numpad_8_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(8)
+
+
+func _on_numpad_9_pointer_event(event):
+	if event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		print(9)
