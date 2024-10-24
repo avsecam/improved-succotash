13dfbc1fcceabec6d1d46dd6e17ef5dc668298a9
kennethkingko <kennethkingko@gmail.com>
2024-10-24 20:45:58 +0800

    Painting placement hotfix

--- src/ois/painting_placement.gd
+++ src/ois/painting_placement.gd
@@ -4,13 +4,15 @@ signal painting_placed
 
 var anim_check : bool
 @onready var progress_view = $"Progress View"
-@onready var frame_3 = $Frame3
+@onready var canvas_ahrt = $Canvas_Ahrt
+@onready var painting_place = $PaintingPlace
 
-func _on_frame_3_has_picked_up(what):
-	if !anim_check:
+
+func _on_test_area_entered(area):
+	if !anim_check and area.name == "CanvasPaintReceiver":
+		canvas_ahrt.visible = true
+		area.get_parent().queue_free()
 		painting_placed.emit()
 		progress_view.visible = true
 		progress_view.progress_complete_checkmark_only_anim()
 		anim_check = true
-		frame_3.enabled = false
-		
