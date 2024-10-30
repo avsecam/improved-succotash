d75124e9659d698e6d32d47b39c4a5475da0478a
kennethkingko <kennethkingko@gmail.com>
2024-10-30 11:54:07 +0800

    added events for powerplant fixing

--- src/assets/B1/GeneratorInteractible.gd
+++ src/assets/B1/GeneratorInteractible.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var anim = $AnimationPlayer
 @export var closed = true
 @export var item:XRToolsPickable
+@export var item_collider:CollisionShape3D
 
 func _on_top_lever_area_pointer_event(event):
 	if event.event_type == XRToolsPointerEvent.Type.PRESSED and closed:
 		anim.play("Crank_Movement")
 		closed = false
 		item.enabled = true
+		item.sleeping = false
+		item_collider.disabled = false
 
 func _on_bottom_lever_area_pointer_event(event):
 	if event.event_type == XRToolsPointerEvent.Type.PRESSED and not closed:
---
+++ src/ems/eventsystem_new/ActionLookAtWaterSpiritB1.gd
@@ -0,0 +1,12 @@
+extends Event
+
+@onready var visible_on_screen_notifier_3d = $"../../WaterSpirit_Normal/VisibleOnScreenNotifier3D"
+
+func _on_event_started() -> void:
+	clear_event_dialogue()
+	visible_on_screen_notifier_3d.screen_entered.connect(_on_visible_on_screen_notifier_3d_screen_entered)
+	await get_tree().create_timer(3.0).timeout
+	play_event_audio()
+
+func _on_visible_on_screen_notifier_3d_screen_entered():
+	close_event()
