fc299c14fd070480605d05575ed6d7233c9e2d48
kennethkingko <kennethkingko@gmail.com>
2024-11-06 18:43:57 +0800

    B2 experience improvements, up for testing + minor bug fixes
    - + Bit more change to the progress complete animation

--- src/areas/special_room_kitchen_panorama_test.gd
+++ src/areas/special_room_kitchen_panorama_test.gd
@@ -1,12 +1,15 @@
 extends Panorama
 
+@onready var shelf = get_node("/root/Demo/Shelf")
+
 func _physics_process(delta):
 	if Engine.is_editor_hint():
 		return
 
 func _ready():
 	super()
+	shelf.visible = false
 	if camera:
 		self.global_position = camera.global_position
 	else:
-		self.global_position = Vector3(0, 0, 0)
+		self.global_position = Vector3(-1.5, 0, 0)
