d07e3735e477c657123c010ea8a3c23d3b70771a
MagusZealot <gg.jeremiah@gmail.com>
2025-01-28 08:18:15 +0800

    modified howitzer event start

--- src/ems/eventsystem_new/DialogueIRememberThisThing.gd
+++ src/ems/eventsystem_new/DialogueIRememberThisThing.gd
@@ -1,10 +1,10 @@
 extends Event
 @onready var shotgun = $"../../shotgun"
-@onready var howitzer_actual = $"../../Howitzer_Actual"
+@onready var howitzer_actual = $"../../Howitzer"
 
 
 func _on_event_started():
-	howitzer_actual
+	howitzer_actual.visible = true
 	shotgun.queue_free()
 	play_event_audio()
 	await event_audio_done
