32c6b1a03c78d25641aa357defa9b789e8d20256
kennethkingko <kennethkingko@gmail.com>
2025-01-30 02:28:21 +0800

    Weapons level bugfix and howitzer effects improvement.
    - Minor audio fixes too.

--- src/areas/special_room_kitchen_panorama_test.gd
+++ src/areas/special_room_kitchen_panorama_test.gd
@@ -1,6 +1,7 @@
 extends Panorama
 
 @onready var shelf = get_node("/root/Demo/Shelf")
+@onready var teleport_bar_reference = get_tree().get_root().get_node("/root/Demo/XRPlayer/XROrigin3D/RightHand/TeleportTrigger/MeshInstance3D")
 
 func _physics_process(delta):
 	if Engine.is_editor_hint():
@@ -9,6 +10,7 @@ func _physics_process(delta):
 func _ready():
 	super()
 	shelf.visible = false
+	teleport_bar_reference.visible = false
 	if camera:
 		self.global_position = camera.global_position
 	else:
--- src/ems/eventsystem_new/DialogueIRememberThisThing.gd
+++ src/ems/eventsystem_new/DialogueIRememberThisThing.gd
@@ -2,11 +2,14 @@ extends Event
 @onready var shotgun = $"../../shotgun"
 @onready var howitzer_actual = $"../../Howitzer"
 @onready var distortion_crystal = $"../../DistortionCrystal10"
+@onready var distortion_light = $"../../DistortionLight"
 
 
 func _on_event_started():
 	howitzer_actual.visible = true
 	shotgun.queue_free()
+	distortion_crystal.visible = true
+	distortion_light.visible = true
 	play_event_audio()
 	await event_audio_done
 	close_event()
