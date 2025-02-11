b616c74841b814103344c529872ed91125c9936b
MagusZealot <gg.jeremiah@gmail.com>
2025-02-12 06:29:24 +0800

    edited to have original rotation of objects

--- src/ois/BindItemLocation.gd
+++ src/ois/BindItemLocation.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var original_position: Vector3
+var original_rotation: Quaternion 
 @onready var parent
 @export var bounds_y = -1.450
 @export var bounds_x = 3
@@ -10,6 +11,7 @@ func _ready():
 	if get_parent():
 		parent = get_parent()
 		original_position = get_parent().global_transform.origin
+		original_rotation = get_parent().global_transform.basis.get_rotation_quaternion()
 
 func _process(delta):
 	if parent.global_transform.origin.y < bounds_y or abs(parent.global_transform.origin.x) > bounds_x or abs(parent.global_transform.origin.z) > bounds_z:
@@ -21,3 +23,4 @@ func _process(delta):
 func reset_parent_position():
 	if get_parent():
 		parent.global_transform.origin = original_position
+		parent.global_transform.basis = Basis(original_rotation)
